09c9ea79302fa5ff93852c06d1245617102ef308
ppw0021 <ppw0021@autuni.ac.nz>
2024-03-13 14:16:09 +1300

    Created Raspberry Pi Backend and HTTP Unity Communication

---
+++ SDP_PROJECT/Assets/InterfaceAPI.cs
@@ -0,0 +1,46 @@
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class InterfaceAPI : MonoBehaviour
+{
+    //public TextMeshProUGUI text;
+    public TextMeshPro text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        StartCoroutine(GetRequest("http://penushost.ddns.net:686/api"));
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class UserList
+    {
+        public List<string> users { get; set; }
+    }
+
+    
+
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(string.Format("Something went wrong: {0}", webRequest.error));
+                    break;
+                case UnityWebRequest.Result.Success:
+                    text.text = webRequest.downloadHandler.text;
+                    break;
+            }
+        }
+    }
+}
