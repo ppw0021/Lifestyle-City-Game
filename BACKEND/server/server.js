4aefa5ed59902bc6495b8a761fd8c7d2390dbae4
ppw0021 <ppw0021@autuni.ac.nz>
2024-03-15 23:23:48 +1300

    Got Official CA Keys on Raspberry PI

--- BACKEND/server/server.js
+++ BACKEND/server/server.js
@@ -1,10 +1,34 @@
-const express = require('express')
+const express = require('express');
+const https = require('https');
+//const path = require('path');
+const fs = require('fs');
 
-const app = express()
+const app = express();
 
+app.get("/", (req, res) => {
+    res.json({ "testing": ["one", "two", "three"] })
+})
 
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] })
 })
 
-app.listen(686, () => { console.log("Server started on port 686") })
+const sslServer = https.createServer(
+    {
+        //How to get trusted Certificate Authority Certificate
+
+
+        //How to generate own SSL CA cert
+        //https://www.youtube.com/watch?v=USrMdBF0zcg
+        //key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+        //cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+
+        cert: fs.readFileSync('/cert/fullchain1.pem'),
+        key: fs.readFileSync('/cert/privkey1.pem')
+    },
+    app
+)
+
+sslServer.listen(443, () => console.log('HTTPS: 443'));
+//app.listen(80, () => console.log("HTTP: 80"));
+//app.listen(686, () => { console.log("Server started on port 686") })
