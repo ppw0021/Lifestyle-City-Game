0c9c5ca6769e5d3d3e89ff9f12edbdc7d0a372e0
hallowseph <146803230+hallowseph@users.noreply.github.com>
2024-04-29 10:33:11 +1200

    Update PlantItem.cs
    now references the FarmManager

--- MAIN_PROJECT/Assets/Crop Growth Assets/PlantItem.cs
+++ MAIN_PROJECT/Assets/Crop Growth Assets/PlantItem.cs
@@ -7,15 +7,23 @@ public class PlantItem : MonoBehaviour
 {
     public PlantObject plant;
     public Image icon;
+    FarmManager fm;
 
     // Start is called before the first frame update
     void Start()
     {
-        icon.sprite = plant.icon;
+        fm = FindObjectOfType<FarmManager>();
+        InitializeUI();
     }
 
     public void BuyPlant()
     {
         Debug.Log("Bought " + plant.plantName);
+        fm.SelectPlant(this);
+    }
+
+    void InitializeUI()
+    {
+        icon.sprite = plant.icon;
     }
 }
