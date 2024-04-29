3babb4dd66cd9a78b9e867c56b51ab36d39156dc
K3v123 <123932560+K3v123@users.noreply.github.com>
2024-04-29 18:01:20 +1200

    updated gui into main
    updated gui of the exp into "currency"
    reverted my old code to test if exp still works. next need to figure out the backend thing to do like Anderson

--- MAIN_PROJECT/Assets/Scripts/expCtrller.cs
+++ MAIN_PROJECT/Assets/Scripts/expCtrller.cs
@@ -13,6 +13,65 @@ public class expCtrller : MonoBehaviour
     [SerializeField] private float TargetExp;
     [SerializeField] private Image expProgressBar;
 
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CurrentExp += 12;
+        }
+
+        expText.text = CurrentExp + " / " + TargetExp;
+
+        ExpController();
+    }
+
+
+
+    public void ExpController()
+    {
+        lvlText.text = "Level: " + level.ToString();
+        expProgressBar.fillAmount = CurrentExp / TargetExp;
+
+
+
+        if (CurrentExp >= TargetExp)
+        {
+            CurrentExp = CurrentExp - TargetExp;
+            level++;
+            TargetExp += 50;
+        }
+    }
+
+}
+
+
+// before code
+
+
+
+
+
+/*
+
+code without space
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class expCtrller : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI expText;
+    [SerializeField] private TextMeshProUGUI lvlText;
+    [SerializeField] public int level;
+    public float CurrentExp;
+    [SerializeField] private float TargetExp;
+    [SerializeField] private Image expProgressBar;
+
     // Call this method whenever you want to update the experience points
     // e.g., after building a house or completing a mission
     public void UpdateExp(float expGained)
@@ -45,3 +104,12 @@ public class expCtrller : MonoBehaviour
     }
 
 }
+*/
+
+
+
+
+
+
+
+
