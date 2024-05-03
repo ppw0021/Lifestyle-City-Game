6d93e4afd5bb8bffd06ee61f5529ce7a2c999dfc
Nicolas-Kotze <149230589+Nicolas-Kotze@users.noreply.github.com>
2024-05-03 19:05:51 +1200

    Comments updated
    Code comments updated to explain functionality of PlacementSystem and InputManager classes better

--- MAIN_PROJECT/Assets/Scripts/InputManager.cs
+++ MAIN_PROJECT/Assets/Scripts/InputManager.cs
@@ -4,39 +4,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+// Class responsible for handling input events and interactions
 public class InputManager : MonoBehaviour
 {
+    // Reference to the scene camera
     [SerializeField]
     private Camera sceneCamera;
 
+    // Last position of the cursor
     private Vector3 lastPosition;
 
+    // Layer mask for object placement
     [SerializeField]
     private LayerMask placementLayermask;
 
-    public event Action OnClicked, OnExit;
+    // Event invoked when the mouse button is clicked
+    public event Action OnClicked;
 
+    // Event invoked when the escape key is pressed
+    public event Action OnExit;
+
+    // Method called every frame
     private void Update()
     {
+        // Check for left mouse button click and invoke OnClicked event
         if(Input.GetMouseButtonDown(0))
             OnClicked?.Invoke();
+        // Check for escape key press and invoke OnExit event
         if(Input.GetKeyDown(KeyCode.Escape))
             OnExit?.Invoke();
     }
 
+    // Method to check if the pointer is over UI
     public bool IsPointerOverUI()
         => EventSystem.current.IsPointerOverGameObject();
 
+    // Method to get the selected map position based on mouse input
     public Vector3 GetSelectedMapPosition()
     {
+        // Get mouse position
         Vector3 mousePos = Input.mousePosition;
+        // Set mouse position z-coordinate to near clip plane of the scene camera
         mousePos.z = sceneCamera.nearClipPlane;
+        // Cast a ray from the screen point to the scene
         Ray ray = sceneCamera.ScreenPointToRay(mousePos);
         RaycastHit hit;
+        // Perform raycast and check if it hits an object on the placement layer
         if (Physics.Raycast(ray, out hit, 100, placementLayermask))
         {
+            // Update lastPosition with the hit point
             lastPosition = hit.point;
         }
+        // Return the last position
         return lastPosition;
     }
 }
--- MAIN_PROJECT/Assets/Scripts/PlacementSystem.cs
+++ MAIN_PROJECT/Assets/Scripts/PlacementSystem.cs
@@ -4,40 +4,53 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+// Class responsible for managing object placement in the game
 public class PlacementSystem : MonoBehaviour
 {
+    // Offset for adjusting grid positions
     [SerializeField]
     private int xy_offset = 5;
-    
-    [SerializeField]
-    private GameObject mouseIndicator; 
 
+    // Reference to the mouse indicator GameObject
     [SerializeField]
-    private InputManager inputManager; 
+    private GameObject mouseIndicator;
 
+    // Reference to the InputManager
+    [SerializeField]
+    private InputManager inputManager;
+
+    // Reference to the grid
     [SerializeField]
     private Grid grid;
 
+    // Reference to the object database
     [SerializeField]
-    private ObjectDatabaseSO database; 
-    public int selectedObjectIndex =-1; 
+    private ObjectDatabaseSO database;
 
+    // Index of the selected object
+    public int selectedObjectIndex = -1;
+
+    // Reference to the grid visualization GameObject
     [SerializeField]
-    private GameObject gridVisualization; 
+    private GameObject gridVisualization;
 
-    private GridData floorData, furnitureData; 
+    private GridData floorData, furnitureData;
 
+    // Reference to the preview system
     [SerializeField]
-    private PreviewSystem preview; 
+    private PreviewSystem preview;
 
     public SmoothCameraMovement smoothCameraMovement;
     private List<GameObject> placedGameObjects = new();
 
     private Vector3Int lastDetectedPosition = Vector3Int.zero; 
-
+    
+    // Method called when the object is initialized
     private void Start()
     {
+          // Stop any ongoing placement
         StopPlacement(); 
+        // Initialize grid data for floor and furniture
         floorData = new();
         furnitureData = new();
       
@@ -57,6 +70,7 @@ public class PlacementSystem : MonoBehaviour
         //int STRUCID = 0; 
         //PlaceObject(XPOS, YPOS, STRUCID);
 
+        // Initialize InterfaceAPI and place objects from building list
         InterfaceAPI.Initialize(this);
         for (int i = 0; i < InterfaceAPI.buildingList.Count; i++)
         {
@@ -68,24 +82,31 @@ public class PlacementSystem : MonoBehaviour
             //InterfaceAPI.buildingList[i].printDetails();
         }
     }
+    // Method to stop object placement
     public void StopPlacement()
     {
+        // Reset selected object index and hide grid visualization
         selectedObjectIndex = -1; 
         gridVisualization.SetActive(false); 
+        // Stop showing preview and remove event listeners
         preview.StopShowingPreview(); 
         inputManager.OnClicked -= PlaceStructure;
         inputManager.OnExit -= StopPlacement; 
         lastDetectedPosition = Vector3Int.zero; 
     }
+     // Method called every frame
     private void Update()
     {
+        // If no object is selected, return
         if (selectedObjectIndex < 0)
         {
             return; 
         }
+         // Get mouse position in world space and convert to grid position
         Vector3  mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition); 
 
+         // If the grid position has changed, update preview position
         if(lastDetectedPosition != gridPosition)
         {
             bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
@@ -96,37 +117,34 @@ public class PlacementSystem : MonoBehaviour
         }
     }
 
+     // Method to start object placement
     public void StartPlacement(int ID)
     {
+        // Stop any ongoing placement
         StopPlacement();
+         // Find the index of the selected object in the database
         selectedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID); 
+         // If object index is not found, log an error and return
         if (selectedObjectIndex < 0 )
         {
             Debug.LogError($"No ID found {ID}");
             return; 
         }
+        // Show grid visualization and start showing placement preview
         gridVisualization.SetActive(true); 
         preview.StartShowingPlacementPreview(database.objectsData[selectedObjectIndex].Prefab, database.objectsData[selectedObjectIndex].Size);
+         // Add event listeners for placement and stopping placement
         inputManager.OnClicked += PlaceStructure;
         inputManager.OnExit += StopPlacement; 
     }
 
     private void PlaceStructure()
     {
-        
-        Vector3  mousePosition = inputManager.GetSelectedMapPosition();
-        Vector3Int gridPosition = grid.WorldToCell(mousePosition); 
-
-        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
-        if(placementValidity == false)
-        {
-            return; 
-        }
-
         //Deduct coins
         int coinsToSet = InterfaceAPI.getCoins();
         coinsToSet -= database.objectsData[selectedObjectIndex].Cost;
         InterfaceAPI.setCoins(coinsToSet);
+        // If pointer is over UI, return
         if(inputManager.IsPointerOverUI()) 
         {
             return; 
@@ -134,6 +152,16 @@ public class PlacementSystem : MonoBehaviour
 
         //Add XP
         InterfaceAPI.addXp(database.objectsData[selectedObjectIndex].XPGain);
+        Vector3  mousePosition = inputManager.GetSelectedMapPosition();
+        // Get mouse position and convert to grid position
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition); 
+
+        // Check placement validity
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+        if(placementValidity == false)
+        {
+            return; 
+        }
 
         //Place on server
         int xpos_grid_database = gridPosition.x + xy_offset;
@@ -141,26 +169,34 @@ public class PlacementSystem : MonoBehaviour
 
         InterfaceAPI.addBuilding(database.objectsData[selectedObjectIndex].ID, xpos_grid_database, ypos_grid_database);
 
+        // Instantiate and position the object
         GameObject newObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab); 
         newObject.transform.position = grid.CellToWorld(gridPosition);
 
+        // Add the object to the list of placed objects
         placedGameObjects.Add(newObject);
+        // Determine the appropriate grid data based on the object type
         GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ? floorData: furnitureData;
 
+        // Add object data to the grid data
         selectedData.AddObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size, database.objectsData[selectedObjectIndex].ID, placedGameObjects.Count -1 ); 
         Debug.Log("Placed Structure (x,y): (" + gridPosition.x + ", " + gridPosition.z + ") Structure ID: " + selectedObjectIndex);
 
+        // Update preview position and appearance
         preview.UpdatePosition(grid.CellToWorld(gridPosition), false); 
 
+        // Deduct object cost from coins and check if there are enough coins remaining
         int coinCheck = InterfaceAPI.getCoins();
         coinCheck -= database.objectsData[selectedObjectIndex].Cost;
 
         if (coinCheck < 0)
         {
+            // Stop placement if there are not enough coins and signal completion
             StopPlacement();
             smoothCameraMovement.PlacementCompleted();
         }
 
+        // If repeat placement is allowed, return
         if (database.objectsData[selectedObjectIndex].AllowRepeatPlacement)
         {
             return;
@@ -173,42 +209,52 @@ public class PlacementSystem : MonoBehaviour
 
     }
 
+     // Method to check the validity of object placement
     private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
     {
+        // Determine the appropriate grid data based on the object type
         GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ? floorData: furnitureData; 
-
+        // Check if the object can be placed at the given grid position
         return selectedData.CanPlaceObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size); 
     }
-
+    // Method to place an object from the building list
     private void PlaceObject(int XPOS, int YPOS, int STRUCID)
     {
         selectedObjectIndex = STRUCID; 
+        // Check if the selected object index is valid
         if (selectedObjectIndex < 0)
         {
+            // Log an error and return if the index is invalid
             Debug.LogError("Early return because of bad selected object index: " + selectedObjectIndex);
             return; 
         }
 
         // Calculate grid position based on XPOS and YPOS
         Vector3Int gridPosition = new Vector3Int(XPOS, 0, YPOS); // Assuming Y is the vertical axis
-       
+        // Check placement validity at the calculated grid position
         bool placementValidity = CheckPlacementValidity(gridPosition, STRUCID);
         if (placementValidity == false)
         {
+            // Log an error and return if placement is not valid
             Debug.LogError("Early Return because of Bad placement Validity");
             return; 
         }
-
+        // Instantiate the object from the database prefab
         GameObject newObject = Instantiate(database.objectsData[STRUCID].Prefab); 
+        // Position the object at the calculated grid position
         newObject.transform.position = grid.CellToWorld(gridPosition);
 
+        // Add the object to the list of placed objects
         placedGameObjects.Add(newObject);
+        // Determine the appropriate grid data based on the object type
         GridData selectedData = database.objectsData[STRUCID].ID == 0 ? floorData : furnitureData;
-
+        // Add object data to the grid data
         selectedData.AddObjectAt(gridPosition, database.objectsData[STRUCID].Size, database.objectsData[STRUCID].ID, placedGameObjects.Count - 1); 
 
         //Debug.Log($"Grid position is: " + gridPosition); 
         //Debug.Log($"Structure ID is: " + selectedObjectIndex); 
+
+        // Log the loaded structure details
         Debug.Log("Loaded Structure (x,y): (" + gridPosition.x + ", " + gridPosition.z + ") Structure ID: " + selectedObjectIndex);
     }
 
