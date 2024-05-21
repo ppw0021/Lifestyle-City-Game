021c9fd1fa4f40abc18c37d4f098334325c98e00
Joseph Reanzares <xxg8089@autuni.ac.nz>
2024-05-21 17:03:36 +1200

    Update CameraZoom.cs
    Added a comment for the minigame camera controls.
    
    Scroll wheel for zoom in/out
    Click and drag right click for panning the camera

--- MAIN_PROJECT/Assets/Scripts/CameraZoom.cs
+++ MAIN_PROJECT/Assets/Scripts/CameraZoom.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script is for the minigame camera controls//
 public class CameraZoom : MonoBehaviour
 {
     // Zoom variables
