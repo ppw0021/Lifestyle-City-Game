b7c4f49a972fe9ec78d1c36b4a65ba9c246decca
K3v123 <123932560+K3v123@users.noreply.github.com>
2024-06-01 16:00:50 +1200

    figured out overlay code.
    cant test it..... the server seems down or something. have to wait for product owner to put back server so i can actually test it. and also take screenshots of tutorial and update it.
    in case if he changed his mind, as well, so i didn't overwrite the previous code, just commented it below in case if i needed it.

--- MAIN_PROJECT/Assets/Scripts/ToTutorial.cs
+++ MAIN_PROJECT/Assets/Scripts/ToTutorial.cs
@@ -12,3 +12,27 @@ public class ToTutorial : MonoBehaviour
         SceneManager.LoadScene("TutorialSection");
     }
 }
+
+/*
+if want overlay
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ToTutorial : MonoBehaviour
+{
+    public GameObject tutorialPanel;  // Assign tutorial panel in the Inspector
+
+    // This method is called when the tutorial button is clicked
+    public void OnTutorialButtonClick()
+    {
+        // Toggle the active state of the tutorial panel
+        tutorialPanel.SetActive(!tutorialPanel.activeSelf);
+    }
+}
+
+
+*/
