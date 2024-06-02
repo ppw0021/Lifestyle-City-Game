6314d11f923da59c69815e077facb63a894e3c2a
Nicolas-Kotze <149230589+Nicolas-Kotze@users.noreply.github.com>
2024-06-03 09:38:50 +1200

    Added animals and changed behavior
    Added more animals, finished UI panel for selecting animals and created a generalized behavior for animal movement.

--- MAIN_PROJECT/Assets/Scripts/AnimalBehavior.cs
+++ MAIN_PROJECT/Assets/Scripts/AnimalBehavior.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BearBehavior : MonoBehaviour
+public class AnimalBehavior : MonoBehaviour
 {
+    public float minWalkDuration = 2f;
+    public float maxWalkDuration = 5f;
+    public float minIdleDuration = 1f;
+    public float maxIdleDuration = 3f;
     public float walkSpeed = 2f;
-    public float idleDuration = 2f;
-    public float walkDuration = 3f;
 
     private Animator animator;
     private bool isWalking = false;
@@ -15,28 +17,40 @@ public class BearBehavior : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        timer = idleDuration;
+        SetRandomTimer();
     }
 
     void Update()
     {
         timer -= Time.deltaTime;
-        
+
         if (timer <= 0)
         {
             isWalking = !isWalking;
             animator.SetBool("isWalking", isWalking);
-            timer = isWalking ? walkDuration : idleDuration;
+            SetRandomTimer();
         }
 
         if (isWalking)
         {
-            // Move the bear forward
+            // Move the animal forward
             transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
 
             // Optionally, add random turning
             float randomTurn = Random.Range(-1f, 1f);
             transform.Rotate(0, randomTurn, 0);
+        }
+    }
+
+    void SetRandomTimer()
+    {
+        if (isWalking)
+        {
+            timer = Random.Range(minWalkDuration, maxWalkDuration);
+        }
+        else
+        {
+            timer = Random.Range(minIdleDuration, maxIdleDuration);
         }
     }
 }
