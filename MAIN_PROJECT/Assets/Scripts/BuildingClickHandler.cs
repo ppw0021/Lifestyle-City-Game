0b890c65983bb61cfa0feafad0090569d75efbeb
Nicolas-Kotze <149230589+Nicolas-Kotze@users.noreply.github.com>
2024-06-04 18:42:08 +1200

    Transparency on Click
    Added a feature to make building transparent when clicked. This will serve to indicate when the building has been selected

--- MAIN_PROJECT/Assets/Scripts/BuildingClickHandler.cs
+++ MAIN_PROJECT/Assets/Scripts/BuildingClickHandler.cs
@@ -4,28 +4,59 @@ using UnityEngine;
 
 public class BuildingClickHandler : MonoBehaviour
 {
-    // This method is called when the object with a collider is clicked
-    private void OnMouseDown()
+    private BuildingInformation buildingInfo;
+    private Renderer objectRenderer;
+    private bool isTransparent = false;
+
+    public Material transparentMaterial; // Assign this in the Inspector
+    private Material originalMaterial;
+
+    private void Start()
     {
         // Get the parent object
         GameObject parentObject = transform.parent.gameObject;
         
         // Get the BuildingInformation component from the parent object
-        BuildingInformation buildingInfo = parentObject.GetComponent<BuildingInformation>();
+        buildingInfo = parentObject.GetComponent<BuildingInformation>();
 
+        // Get the Renderer component from the child object (this object)
+        objectRenderer = GetComponent<Renderer>();
+
+        // Store the original material
+        if (objectRenderer != null)
+        {
+            originalMaterial = objectRenderer.material;
+        }
+    }
+
+    private void OnMouseDown()
+    {
         if (buildingInfo != null)
         {
-            Debug.Log("Building Information:");
-            Debug.Log("X Position: " + buildingInfo.GetXPOS());
-            Debug.Log("Y Position: " + buildingInfo.GetYPOS());
-            Debug.Log("Structure ID: " + buildingInfo.GetSTRUCID());
-            Debug.Log("Instance ID: " + buildingInfo.GetINSTID());
+            Debug.Log($"Building Information: X Position: {buildingInfo.GetXPOS()}, Y Position: {buildingInfo.GetYPOS()}, Structure ID: {buildingInfo.GetSTRUCID()}, Instance ID: {buildingInfo.GetINSTID()}");
         }
         else
         {
             Debug.LogError("BuildingInformation component not found on parent object!");
         }
+
+        if (objectRenderer != null)
+        {
+            ToggleTransparency();
+        }
+    }
+
+    private void ToggleTransparency()
+    {
+        if (isTransparent)
+        {
+            objectRenderer.material = originalMaterial;
+        }
+        else
+        {
+            objectRenderer.material = transparentMaterial;
+        }
+
+        isTransparent = !isTransparent;
     }
 }
-
-
